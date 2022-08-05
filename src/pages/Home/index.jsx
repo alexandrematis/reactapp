2ba55052e5cc62b88daa63de720508b280d19902
Alexandre Matis <alexandrematis@gmail.com>
2022-08-05 14:02:30 -0300

    Funcionalidade com useState e map

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,53 @@
+import React, {useState, useEffect} from 'react'
 import './styles.css'
 
 import { Card } from '../../components/Card'
 
 export function Home() {
+  const [studentName, setStudentName] = useState("")
+  const [students, setStudents] = useState([])
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br",{
+        hour: "2-digit", 
+        minute: "2-digit", 
+        second: "2-digit"
+      })
+    }
+
+    setStudents(prevState => [...prevState,newStudent])
+  }
+ 
   return (
   <div className="container">
-    
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite o nome..." />
-    <button>Adicionar</button>
+    <header>
+      <h1>Lista de Presença</h1>
+      <div>
+        <strong>Alexandre Matis</strong>
+        <img src="https://github.com/alexandrematis.png" alt="Alexandre Matis" />
+      </div>
+    </header>
 
-    <Card name="Rodrigo" time="10:55:24" />
-    <Card name="João" time="11:00:10" />
-    <Card name="Ana" time="12:10:33" />
+    <input 
+      type="text" 
+      placeholder="Digite o nome..."
+      onChange={e => setStudentName(e.target.value)}
+    />
+    
+    <button type="button" onClick={handleAddStudent}>
+        Adicionar
+    </button>
+
+    {
+    students.map(student => 
+      <Card
+        key={student.time}
+        name={student.name} 
+        time={student.time}
+       />
+    )}
     
   </div>
   )
